e9b649498dc7dff8579a7056417e2e14334501d8
derftymutt <derftymutt@gmail.com>
2017-02-22 20:38:40 -0800

    added BaseController

---
+++ src/AppleBrainsSite/AppleBrainsSite/Scripts/Ng/BaseController.js
@@ -0,0 +1,30 @@
+﻿(function () {
+
+    angular.module('app').factory('$baseController', BaseController);
+
+
+    BaseController.$inject = ['$document', '$log', '$route', '$routeparams', '$systemEventService', '$alertService'];
+
+    function BaseController(
+        $document
+        , $log
+        , $route
+        , $routeparams
+        , $systemEventService
+        , $alertService) {
+
+        var base = {
+            $document : $document,
+            $log : $log,
+            $route: $route,
+            $routeparams: $routeparams,
+            $systemEventService : $systemEventService,
+            $alertService: $alertService,
+            merge : $.extend
+        }
+
+        return base;
+
+        }
+
+}());
